ef8d67af90b56ae0c31cc995c130bf484900e48b
Sherzod <sherzodakramov1999@gmail.com>
2023-02-08 22:21:35 +0500

    Edit main information about author

--- send_email.py
+++ send_email.py
@@ -1,11 +1,13 @@
-import smtplib, ssl
+import os
+import smtplib
+import ssl
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "sherzodakramov1999@gmail.com"
-    password = "ysoaovjzgcaiiocs"
+    password = os.getenv("PASSWORD")
     receiver = "sherzodakramov1999@gmail.com"
     # It holds secure context for sending email securely
     context = ssl.create_default_context()
@@ -13,7 +15,3 @@ def send_email(message):
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
-
-
-
-
